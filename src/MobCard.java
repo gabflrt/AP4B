8eabfee324b345afa1a4d60679b3b33a037b9a90
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-12-06 22:50:17 +0100

    Adding basic things

--- src/MobCard.java
+++ src/MobCard.java
@@ -1,3 +1,30 @@
 public class MobCard extends Card {
+    private int strength;
+    MobCard(){
+        super();
+        this.strength = 0;
+    }
 
+    MobCard(String name, String description, int strength){
+        super(name, description);
+        this.strength = strength;
+    }
+
+    /**
+     * Setter for the strength of a mob
+     *
+     * @param strength the strength of a mob
+     */
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    /**
+     * Getter for the strength of a mob
+     *
+     * @return the strength of a mob
+     */
+    public int getStrength() {
+        return this.strength;
+    }
 }
