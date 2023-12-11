9e5e230dcce8adf8ccd05d4bb33684632449a679
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-12-11 22:22:06 +0100

    Adding a toString method for the deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Deck extends CardPile {
     /**
      * This method will give the 2 cards that a deck must have at the beginning of
      * the game.
      *
-     * @param drawPile the draw pile from which we will draw the 12 cards
+     * @param drawPileDungeon  the draw pile of the dungeon
+     * @param drawPileTreasure the draw pile of the treasure
      * @return an ArrayList with 2 cards.
      */
     public ArrayList<Card> initializeDeck(DrawPile drawPileDungeon, DrawPile drawPileTreasure) {
@@ -18,4 +20,16 @@ public class Deck extends CardPile {
         return this.getCardPile();
     }
 
+    /**
+     * This method is a toString method that will print the deck of the player.
+     *
+     * @return a string with the deck of the player.
+     */
+    public String toString() {
+        System.out.println("The deck of the player is : ");
+        for (int i = 0; i < this.getCardPile().size(); i++) {
+            System.out.println(this.getCardPile().get(i));
+        }
+        return "";
+    }
 }
