51e3ad62051095e5e6bfaa7e4f757d0f834e8c14
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-12-13 09:49:31 +0100

    I've made the ObjectCard class.

--- src/ObjectCard.java
+++ src/ObjectCard.java
@@ -1,3 +1,30 @@
 public class ObjectCard extends Card {
+    private int strengthBonus;
+
+    ObjectCard() {
+        super();
+        this.strengthBonus = 0;
+    }
+
+    ObjectCard(String name, String description, int strengthBonus) {
+        super(name, description);
+        this.strengthBonus = strengthBonus;
+    }
+
+    /**
+     * Getter for the strength bonus of an object
+     * @return the strength bonus of an object
+     */
+    public int getStrengthBonus() {
+        return this.strengthBonus;
+    }
+
+    /**
+     * Setter for the strength bonus of an object
+     * @param strengthBonus the strength bonus of an object
+     */
+    public void setStrengthBonus(int strengthBonus) {
+        this.strengthBonus = strengthBonus;
+    }
 
 }
