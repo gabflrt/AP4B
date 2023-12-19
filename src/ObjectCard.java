762d0b339841164da714e3a71a6fa7cf02a481f4
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-12-19 21:35:54 +0100

    - Adding variable ClassCondition which is a condition to use an object card + the  getter for this variable.

--- src/ObjectCard.java
+++ src/ObjectCard.java
@@ -1,14 +1,17 @@
 public class ObjectCard extends Card {
     private int strengthBonus;
+    private String classeCondition;
 
     ObjectCard() {
         super();
         this.strengthBonus = 0;
+        this.classeCondition = "";
     }
 
-    ObjectCard(String name, String description, int strengthBonus) {
+    ObjectCard(String name, String description, int strengthBonus, String classeCondition) {
         super(name, description);
         this.strengthBonus = strengthBonus;
+        this.classeCondition = classeCondition;
     }
 
     /**
@@ -25,6 +28,14 @@ public class ObjectCard extends Card {
      */
     public void setStrengthBonus(int strengthBonus) {
         this.strengthBonus = strengthBonus;
+    }
+
+    /**
+     * Getter for the class condition of an object
+     * @return the class condition of an object
+     */
+    public String getClasseCondition() {
+        return this.classeCondition;
     }
 
 }
