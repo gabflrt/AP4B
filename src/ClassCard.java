016c82f99a471d4ed9f31b2b5d61868830c41fb9
SuperTimCraft <92984690+SuperTimCraft@users.noreply.github.com>
2023-12-19 21:38:19 +0100

    Creating the basis of ClassCard with the name of a class + setter & getter for that

--- src/ClassCard.java
+++ src/ClassCard.java
@@ -1,3 +1,30 @@
-public class ClassCard {
-    
+public class ClassCard extends Card {
+
+    private String classe;
+
+    ClassCard() {
+        super();
+        this.classe = "";
+    }
+
+    ClassCard(String name, String description, String classe) {
+        super(name, description);
+        this.classe = classe;
+    }
+
+    /**
+     * Getter for the class of a player
+     * @return the class of a player
+     */
+    public String getClasse() {
+        return this.classe;
+    }
+
+    /**
+     * Setter for the class of a player
+     * @param classe the class of a player
+     */
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
 }
